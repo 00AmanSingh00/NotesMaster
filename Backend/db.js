304eafe2e44b8d3028c03607ed2e29a82e3bce47
00AmanSingh00 <amansinghh9408@gmail.com>
2025-02-22 22:36:59 +0530

    Fixed vercel.json and db.js issues

--- Backend/db.js
+++ Backend/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-const connectToMongo = () => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-        .then(() => {
-            console.log('Connected to Mongo Successfully')
-        })
-        .catch(console.error())
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+        console.log('Connected to Mongo Successfully');
+    } catch (error) {
+        console.error('MongoDB Connection Error:', error);
+    }
 }
 
 module.exports = connectToMongo;
